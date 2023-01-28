96042ad0be731144363f98e4d82bced45aaf5bce
John Griffiths <gnuchu@gmail.com>
2023-01-28 15:19:30 +0000

    Working day3 part2

--- day3/part2/part2.py
+++ day3/part2/part2.py
@@ -1,6 +1,7 @@
 import os.path
 import pathlib 
 import os
+import sys
 
 def read_data(path):
     data = ""
@@ -13,38 +14,36 @@ def get_data_file_path():
     data_file_path = os.path.join(current_path, 'data/data.txt')
     return data_file_path
 
-def get_test_data_file_path():
+def get_test_data_file_path(part):
     current_path = pathlib.Path(__file__).parent.parent
-    data_file_path = os.path.join(current_path, 'data/test_data_part1.txt')
+    data_file_path = os.path.join(current_path, f"data/test_data_part{part}.txt")
     return data_file_path
 
+def process_old_words(old_words):
+    # old_word [] of three words that will have one common badge between them.
+    
+    badge = common_elements(old_words[0], old_words[1], old_words[2])[0]
+    if is_lowercase(badge):
+        return is_lowercase_priority(badge)
+    else:
+        return is_uppercase_priority(badge)
+
 def process_data(data):
-    grand_total = 0
+    words = []
+    total = 0
+    old_words = []
 
     for n, line in enumerate(data.split("\n")):
-        if (n+1%3 == 0):
-          old_words = words
-          words = []
-          words.append(line)
-          word1 = words[0]
-          word2 = words[1]
-          word3 = words[2]
-          common_elements = commoon_element
+        no = n+1
+        if (no%3 == 0):
+            words.append(line)
+            old_words = words
+            total += process_old_words(old_words)
+            words = []
         else:
-          words.append(line)
-
-        common_elements = commoon_element(word1, word2)
-
-        print(f"Word1: {word1}, Word2: {word2}, Common Elements: {common_elements}")
-
-        for i in common_elements:
-            if is_uppercase(i):
-                grand_total += is_uppercase_priority(i)
-            elif is_lowercase(i):
-                grand_total += is_lowercase_priority(i)
-
-    print(f"The grand total is: {grand_total}")
-
+            words.append(line)
+    
+    print(f"Total = {total}")
 
 def is_lowercase_priority(character):
     return int(ord(character) - 96)
@@ -75,9 +74,10 @@ def common_elements(str1, str2, str3):
 
 
 if __name__ == "__main__":
+    part = 2
     try:
         if os.environ['TEST'] is not None:
-            path = get_test_data_file_path()
+            path = get_test_data_file_path(part=part)
     except:
         path = get_data_file_path()
 
