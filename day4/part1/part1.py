b3609d311a37c9b75692f37265266af1f9a8d1b8
John Griffiths <gnuchu@gmail.com>
2023-01-30 23:16:42 +0000

    start of day4

---
+++ day4/part1/part1.py
@@ -0,0 +1,37 @@
+import os.path
+import pathlib 
+import os
+
+def read_data(path):
+  data = ""
+  with open(path, "r") as f:
+    data = f.read()
+  return data
+
+def get_data_file_path():
+  current_path = pathlib.Path(__file__).parent.parent
+  data_file_path = os.path.join(current_path, 'data/data.txt')
+  return data_file_path
+
+def get_test_data_file_path(part):
+    current_path = pathlib.Path(__file__).parent.parent
+    data_file_path = os.path.join(current_path, f"data/test_data_part{part}.txt")
+    return data_file_path
+
+def process_data(data):
+    for n, line in enumerate(data.split("\n")):
+        print(f"{n}: {line}")
+
+
+if __name__ == "__main__":
+  part = FAIL_TO_COMPILE_IF_NOT_ENTERED
+  
+  try:
+      if os.environ['TEST'] is not None:
+          path = get_test_data_file_path()
+  except:
+      path = get_data_file_path()
+
+  data = read_data(path)
+
+  process_data(data)
